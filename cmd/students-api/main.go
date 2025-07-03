98f529000e076d1f440c48eb48caf5744e16a090
ranit1803 <ranitsantra.sssnc@gmail.com>
2025-07-03 20:36:39 +0530

    Loaded the Config, Setting up Router and Server

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,5 +1,60 @@
 package main
-import "fmt"
+
+import (
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/ranit1803/students-api/internal/config"
+)
+
+
 func main(){
-	fmt.Println("This is the start!")
+	/*
+	the main things in main to do is
+	1) load the config
+	2) setup the database
+	3) setup the router
+	4) setup the server
+	*/
+	//Loading the config
+	cfg:=config.MustLoad()
+
+	//Setting up the router
+	router:= http.NewServeMux()
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to this project!"))
+	})
+
+	//Setup the Server
+	server:= http.Server{
+		Addr: cfg.HTTPServer.Address,
+		Handler: router,
+	}
+	slog.Info("Server Listening at",slog.String(": ",cfg.HTTPServer.Address))
+
+	channel_shutdown:= make(chan os.Signal, 1)
+	signal.Notify(channel_shutdown,os.Interrupt,syscall.SIGINT,syscall.SIGABRT,syscall.SIGTERM)
+	go func ()  {
+		err:= server.ListenAndServe() // without goroutine i cannot graceful shutdown
+		if err!= nil{
+			log.Fatal("Failed to Serve!")
+	}
+	}()
+	<- channel_shutdown
+
+	//Stopping the server
+	slog.Info("Server Stopping..")
+	ctx, cancel:= context.WithTimeout(context.Background(),time.Second * 5)
+	err:= server.Shutdown(ctx)
+	if err!= nil{
+		slog.Error("Failed to Shut Down",slog.String("Error",err.Error()))
+	}
+	defer cancel()
+	slog.Info("Server Shutdown Successfully")
 }
